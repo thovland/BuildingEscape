6aad69f8a668f2ab48fac196a9e827883a7769c1
Thomas <thovlands@gmail.com>
2018-10-02 14:20:44 +0200

    078 debugline

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -4,6 +4,7 @@
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
+#include "DrawDebugHelpers.h"
 
 #define OUT
 
@@ -42,10 +43,26 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	);
 
 	// TODO Log out to test
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Position: %s"), 
+	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Position: %s"), 
 		*PlayerViewPointLocation.ToString(),
-		*PlayerViewPointRotation.ToString()
-	)
+		PlayerViewPointRotation.ToString()
+	)*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() *Reach;
+
+	// Draw a red trace in the world to visualize
+
+	DrawDebugLine(
+
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		5.f
+	);
 
 	// Ray-cast out to reach distance
 
