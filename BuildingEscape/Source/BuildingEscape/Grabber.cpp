7cb46fce83fa831ab3cf88205880e3525bbf3b2f
Thomas <thovlands@gmail.com>
2018-10-08 14:13:19 +0200

    093 pointers protected

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -60,6 +60,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	if (!PhysicsHandle) { return; }
 	//if the physics handle is attached
 	if (PhysicsHandle->GrabbedComponent)
 	{
@@ -79,6 +80,7 @@ void UGrabber::Grab()
 		//If we hit something then attach a physics handle
 		if (ActorHit != nullptr)
 		{
+			if (!PhysicsHandle) { return; }
 				PhysicsHandle->GrabComponent(
 					ComponentToGrab, 
 					NAME_None, //no bones needed
@@ -90,6 +92,7 @@ void UGrabber::Grab()
 
 void UGrabber::Release()
 {
+	if (!PhysicsHandle) { return; }
 		PhysicsHandle->ReleaseComponent();
 }
 
--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -25,6 +25,10 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 	Owner = GetOwner();
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing pressure plate"), *GetOwner()->GetName())
+	}
 }
 
 void UOpenDoor::OpenDoor()
@@ -64,6 +68,7 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 	
 	// Find all the overlapping actors
 	TArray<AActor*> OverlappingActors;
+	if (!PressurePlate) { return TotalMass; }
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
 
 	// Iterate through them adding their masses 
